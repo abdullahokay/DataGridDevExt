653995898d2efd33eeabba527989eea6a136c136
abdullahokay <107299543+abdullahokay@users.noreply.github.com>
2023-06-09 20:38:57 +0300

    improvements

--- DataGrid/src/services/crud.js
+++ DataGrid/src/services/crud.js
@@ -1,8 +1,10 @@
 import DataSource from 'devextreme/data/data_source';
 import LocalStore from 'devextreme/data/local_store';
 
+//data grid için local storage' da hali hazırda (varsa) var olan veriler alındı
 var storageData = JSON.parse(localStorage.getItem("dx-data-localStore-socials"));
 
+//data grid için local storage' da alan ayırıldı ve data gridin data source' u storageData değişkeni olarak belirlendi
 const dataSource = new DataSource({
   store: new LocalStore({
     key: 'ID',
